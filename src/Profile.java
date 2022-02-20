6850b6553ca148dbccc130f7574d52c08e517c4d
Tyler Sarno <tylersarno21@gmail.com>
2022-02-20 15:31:39 -0500

    update

--- src/Profile.java
+++ src/Profile.java
@@ -1,11 +1,17 @@
+/**
+ * @author Tyler Sarno
+ * This class defines the profile for a banking customer
+ */
 public class Profile {
     private String fname;
     private String lname;
     private Date dob;
 
+    //Constructor for the profile object
     public Profile (String fname, String lname, Date dob){
         this.fname = fname;
         this.lname = lname;
         this.dob = dob;
     }
+
 }
