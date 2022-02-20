65cf253e505fbe85521a1f224d1b651de1e07181
Tyler <tylersarno21@gmail.com>
2022-02-20 15:52:50 -0500

    formatted date

--- src/Date.java
+++ src/Date.java
@@ -1,27 +1,45 @@
 import java.util.Calendar;
 import java.util.StringTokenizer;
+
 /**
- This class defines the date object, as well as methods to use with it.
- @author Aum Pathak
+ * This class defines the date object, as well as methods to check its validity, etc.
+ *
+ * @author Aum Pathak, Tyler Sarno
  */
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
 
+    /*
+    getter method for day
+    @return int value for day
+     */
     public int getDay() {
         return this.day;
     }
 
+    /*
+    getter method for month
+    @return the int value for the month
+     */
     public int getMonth() {
         return this.month;
     }
 
+    /*
+    getter method for year
+    @return the int value for the year
+     */
     public int getYear() {
         return this.year;
     }
 
-    public Date(String date) { //take “mm/dd/yyyy” and create a Date object
+    /*
+    Constructor for the date object where the user can specify a certain day.
+    @param takes in the string form of a date
+     */
+    public Date(String date) {
         StringTokenizer s = new StringTokenizer(date, "/");
         if (s.countTokens() != 3) throw new IllegalArgumentException();
         int mm = Integer.parseInt(s.nextToken());
@@ -33,15 +51,22 @@ public class Date implements Comparable<Date> {
         this.day = dd;
     }
 
-    public Date() { //create an object with today’s date (see Calendar class)
+    /*
+    constructor without parameters for the date object
+    it automatically sets the date to today's date
+     */
+    public Date() {
         Calendar TODAY = Calendar.getInstance();
         this.day = TODAY.get(Calendar.DATE);
-        this.month = TODAY.get(Calendar.MONTH)+1;
+        this.month = TODAY.get(Calendar.MONTH) + 1;
         this.year = TODAY.get(Calendar.YEAR);
-
     }
 
-    //checking if the date is a leap year
+    /*
+    Check to see if a certain year is a leap year
+    @param takes in an int value for the year
+    @return true if it is a leap year, false otherwise
+     */
     public static boolean isLeap(int year) {
         if (year % 4 != 0) {
             return false;
@@ -53,7 +78,11 @@ public class Date implements Comparable<Date> {
             return true;
         }
     }
-    // convert date to string
+
+    /*
+    convert date to string
+    @return the date in the form of a string
+    */
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -69,7 +98,10 @@ public class Date implements Comparable<Date> {
         return s.toString();
     }
 
-    //checking if the date is valid
+    /*
+    checking if the date is valid
+    @return true if the date is valid and false otherwise
+     */
     public boolean isValid() {
         if (year < 0) return false;
         if ((month < 1) || (month > 12)) return false;
@@ -81,24 +113,29 @@ public class Date implements Comparable<Date> {
         };
     }
 
-    //compareTo method for Date
+    /*
+    compareTo method for the Date object
+    @param the date you want to compare
+    @return 1 if the date is greater, -1 if the date is less than, and 0
+    if the dates are the same
+     */
     @Override
     public int compareTo(Date date) {
         int day1 = date.getDay();
         int month1 = date.getMonth();
         int year1 = date.getYear();
 
-        if(year1 > this.year)
+        if (year1 > this.year)
             return -1;
-        if(year1 < this.year)
+        if (year1 < this.year)
             return 1;
-        if(month1 > this.month)
+        if (month1 > this.month)
             return -1;
-        if(month1 < this.month)
+        if (month1 < this.month)
             return 1;
-        if(day1 > this.day)
+        if (day1 > this.day)
             return -1;
-        if(day1 < this.day)
+        if (day1 < this.day)
             return 1;
         return 0;
 
@@ -115,7 +152,7 @@ public class Date implements Comparable<Date> {
         System.out.println(today.compareTo(tomorrow)); // today is earlier than tomorrow
 
         // 2013 is not leap year
-        System.out.println(Date.isLeap(2016) ? "is leap": "is not leap");
+        System.out.println(Date.isLeap(2016) ? "is leap" : "is not leap");
 
         Date rando = new Date("21/21/2050");
         System.out.println(rando.isValid());
